00b930960156b4fba815772caebcebe5c96d09e4
yuchan <lumir1031@gmail.com>
2020-12-07 10:49:02 +0800

    warm up answer

--- 01_warm-up-basic/main.js
+++ 01_warm-up-basic/main.js
@@ -1,1 +1,11 @@
-// TO DO
+function addCircle() {
+  let div = document.createElement('div')
+  div.classList.add("circle")
+  return div
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector('button').addEventListener('click', function () {
+    document.querySelector('.box').insertAdjacentElement('beforeend', addCircle())
+  })
+})
