51f03a0545175cad90f640ee417dd880594f4fd0
yuchan <lumir1031@gmail.com>
2021-04-19 12:11:55 +0800

    update question

---
+++ 03_warm-up-basic/main.js
@@ -0,0 +1,31 @@
+// TO DO
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#button').addEventListener('click', (e) => {
+    /*
+    你可能要會知道以下的方法在幹嘛：
+    nextElementSibling
+    previousElementSibling
+    parentNode
+    children
+    firstElementChild
+    lastElementChild
+    */
+
+  /* 題目1: 把包著 內容1 的 div 加上 .pink 的 class  */
+   // to do...
+
+
+  /* 題目2: 把「內容1」加上 .blue class  */
+    // to do...
+
+
+  /* 題目3: 把「內容2」的文字內容改變 */
+    // to do...
+
+
+  /* 題目4 : 把「標題」的文字顏色改變 */
+    // to do...
+
+
+  })
+})
